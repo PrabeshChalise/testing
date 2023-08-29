de060f5779fcc78cd3fd6978cb745fd5b753a0e4
prabeshcc chalise <PrabeshChalise>
2023-08-29 09:33:04 +0545

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 80;
 app.get("/", (req, res) => {
-  res.send("Hello its working");
+  res.send("Hello its working xd");
 });
 app.listen(80);
